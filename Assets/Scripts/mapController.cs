d71e0e727a538b92b8798b48d6703b92cbb0bfb1
Ryan Kramer <kramer545@gmail.com>
2017-03-03 20:53:17 -0800

    First commit, started making board script and tile script

---
+++ Assets/Scripts/mapController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapController : MonoBehaviour {
+
+
+	tile[,] board;
+	teleporter[] teleX;
+	teleporter[] teleY;
+	teleporter[] teleZ;
+	door door1;
+	door door2;
+	door door3;
+	button btnA;
+	button btnB;
+	button btnC;
+	//positions for use of navmesh agent
+	Transform exitPos;
+	Transform startPos;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//read text file, get map dimensions, iterate and make tiles
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void readFile(string filename)
+	{
+		FileStream F = new FileStream (filename, FileMode.Open, FileAccess.Read);
+		int count = 0;
+		while (F.ReadByte () != '\n')
+			count++;
+		F.Position = 0;//reset postion to start of file
+		board = new tile[F.Length/count,count];
+		for(int x = 0;x<F.Length/count;x++)
+		{
+			for(int y = 0;y<count;y++)
+			{
+				board [x,y] = new tile (x, y, F.ReadByte(),this);
+			}
+		}
+	}
+
+	public void setTeleporter(teleporter tele,int type)//assign and link teleporters of same type
+	{
+		//type is if it's X,Y,Z, with 0=x,1=y,2=z
+		if(type == 0)//X
+		{
+			if (teleX [0] != null) {
+				teleX [1] = tele;
+				//link em
+			} else
+				teleX [0] = tele;
+		}
+		else if(type == 1)//y
+		{
+			if (teleY [0] != null) {
+				teleY [1] = tele;
+				//link em
+			} else
+				teleY [0] = tele;
+		}
+		else if(type == 2)//z
+		{
+			if (teleZ [0] != null) {
+				teleZ [1] = tele;
+				//link em
+			} else
+				teleZ [0] = tele;
+		}
+	}
+
+	public void setDoor(door doorObj,int type)//assign and link doors with btns
+	{
+		//type is if it's 1,2,3
+		if(type == 1)
+		{
+			door1 = doorObj;
+			if(btnA != null)
+			{
+				//link em
+			}
+		}
+		else if(type == 2)
+		{
+			door2 = doorObj;
+			if(btnB != null)
+			{
+				//link em
+			}
+		}
+		else if(type == 3)
+		{
+			door3 = doorObj;
+			if(btnB != null)
+			{
+				//link em
+			}
+		}
+	}
+
+	public void setButton(button btn,int type)//assign and link btns to doors
+	{
+		//type is if it's 1,2,3 with 0=a,1=b,2=c
+		if(type == 0)
+		{
+			btnA = btn;
+			if(door1 != null)
+			{
+				//link em
+			}
+		}
+		else if(type == 1)
+		{
+			btnB = btn;
+			if(door2 != null)
+			{
+				//link em
+			}
+		}
+		else if(type == 2)
+		{
+			btnC = btn;
+			if(door3 != null)
+			{
+				//link em
+			}
+		}
+	}
+
+	public void setStart(Transform pos)
+	{
+		startPos = pos;
+	}
+
+	public void setEnd(Transform pos)
+	{
+		exitPos = pos;
+	}
+}
---
+++ Assets/Scripts/tile.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tile : MonoBehaviour {
+
+	mapController controller;
+	int x,y;// 2d coordinates
+	/*
+	 * O = empty, traversable tile
+	 * X = impassible wall
+	 * U/D/L/R = starting location, with facing location
+	 * 1,2,3 = locked doors
+	 * a,b,c = button to unlock corresponding door (a=1,b=2,c=3)
+	 * x,y,z = teleporter, comes in pairs, with each pointing to each other (x->x)
+	 * M = mine, invisible first person, visible on top down map
+	 * E = exit
+	 */
+	char type;
+
+	// Use this for initialization
+	public tile (int x,int y, char type,mapController controller) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.controller = controller;
+		if (!handleType ())
+			Debug.Log ("error setting type: "+type);
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool handleType()//depending on type, spawns and links objects accordingly
+	{
+		type = type.ToUpper();
+		switch(type)
+		{
+		case('O'):
+			{
+				//do nothing
+				break;
+			}
+		case('X'):
+			{
+				//instatiante wall at x,y
+				break;
+			}
+		case('U'):
+			{
+				//spawn player facing up
+				break;
+			}
+		case('D'):
+			{
+				//spawn player facing up
+				break;
+			}
+		case('L'):
+			{
+				//spawn player facing up
+				break;
+			}
+		case('R'):
+			{
+				//spawn player facing up
+				break;
+			}
+		case('1'):
+			{
+				//spawn door, if btn available, link them
+				break;
+			}
+		case('2'):
+			{
+				//spawn door, if btn available, link them
+				break;
+			}
+		case('3'):
+			{
+				//spawn door, if btn available, link them
+				break;
+			}
+		case('A'):
+			{
+				//spawn btn, if door available, link them
+				break;
+			}
+		case('B'):
+			{
+				//spawn btn, if door available, link them
+				break;
+			}
+		case('C'):
+			{
+				//spawn btn, if door available, link them
+				break;
+			}
+		case('X'):
+			{
+				//spawn teleporter, if other x is available, link them
+				break;
+			}
+		case('Y'):
+			{
+				//spawn teleporter, if other y is available, link them
+				break;
+			}
+		case('Z'):
+			{
+				//spawn teleporter, if other z is available, link them
+				break;
+			}
+		case('M'):
+			{
+				//spawn mine
+				break;
+			}
+		case('E'):
+			{
+				//spawn exit
+				break;
+			}
+		default:
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
